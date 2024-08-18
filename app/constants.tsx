67bf3354f296c502b9ded273d407f4ab06a30d0b
Ava Collins <iamsimplesthing@gmail.com>
2024-08-18 12:27:24 -0400

    refactor: images

---
+++ app/constants.tsx
@@ -0,0 +1,90 @@
+import { link } from "fs";
+
+export const worklist = [
+  {
+    title: "WORK",
+    name: "Expedia",
+    description:
+      "Expedia is a travel company that provides travel services and products to customers.",
+    image: "/work/expedia/logo.png",
+    link: "/work/expedia",
+  },
+  {
+    name: "Porch",
+    description:
+      "Porch is a home services platform that connects homeowners with quality home improvement professionals.",
+    image: "/work/porch/logo.png",
+    link: "/work/porch",
+  },
+  {
+    name: "Sansar",
+    description:
+      "Sansar is a social virtual reality platform that allows users to create, share, and monetize their own VR experiences.",
+    image: "/work/sansar/logo.png",
+    link: "/work/sansar",
+  },
+  {
+    name: "Apollo",
+    description:
+      "Apollo Education Group, Inc. is an American corporation that owns several for-profit educational institutions.",
+    image: "/work/apollo/logo.png",
+    link: "/work/apollo",
+  },
+  {
+    name: "Amazon",
+    description:
+      "Amazon is an American multinational technology company that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
+    image: "/work/amazon/logo.png",
+    link: "/work/amazon",
+  },
+  {
+    name: "Nordstrom",
+    description: "Nordstrom is an American luxury department store chain.",
+    image: "/work/nordstrom/logo.png",
+    link: "/work/nordstrom",
+  },
+  {
+    name: "Disney",
+    description:
+      "The Walt Disney Company, commonly known as Disney, is an American diversified multinational mass media and entertainment conglomerate.",
+    image: "/work/disney/logo.png",
+    link: "/work/disney",
+  },
+  {
+    name: "Real Networks",
+    description:
+      "RealNetworks is a provider of Internet streaming media delivery software and services based in Seattle, Washington, United States.",
+    image: "/work/realnetworks/logo.png",
+    link: "/work/realnetworks",
+  },
+];
+
+export const playlist = [
+  {
+    title: "PLAY",
+    name: "Tarot",
+    description: "Tarot is a card game that is played with a deck of 78 cards.",
+    image: "/play/tarot/logo.png",
+    link: "/play/tarot",
+  },
+  {
+    name: "Federated Modules",
+    description:
+      "Federated Modules is a new feature in Webpack 5 that allows you to split your code into multiple bundles that can be loaded on demand.",
+    image: "/play/federated-modules/logo.png",
+    link: "/play/federated-modules",
+  },
+  {
+    name: "Sansar Scripts",
+    description:
+      "Sansar Scripts is a C# scripting language for creating interactive experiences in Sansar.",
+    image: "/work/sansar/logo.png",
+    link: "/play/sansar-scripts",
+  },
+  {
+    name: "SFJS",
+    description: "SFJS was a JavaScript meetup group in Seattle.",
+    image: "/play/sfjs/logo.png",
+    link: "/play/sfjs",
+  },
+];
--- app/work/page.tsx
+++ app/work/page.tsx
@@ -1,111 +1,52 @@
 import styles from "./page.module.css";
 import Image from "next/image";
 import React from "react";
+import { playlist, worklist } from "../constants";
 
-type WorkItem = {
-  title: string;
+type Item = {
+  title?: string;
+  name: string;
   description: string;
   image: string;
   link: string | undefined;
 };
 
-const worklist = [
-  {
-    title: "Expedia",
-    description:
-      "Expedia is a travel company that provides travel services and products to customers.",
-    image: "expedia.png",
-    link: "/work/expedia",
-  },
-  {
-    title: "Porch",
-    description:
-      "Porch is a home services platform that connects homeowners with quality home improvement professionals.",
-    image: "porch.png",
-    link: "/work/porch",
-  },
-  {
-    title: "Sansar",
-    description:
-      "Sansar is a social virtual reality platform that allows users to create, share, and monetize their own VR experiences.",
-    image: "sansar.png",
-    link: "/work/sansar",
-  },
-  {
-    title: "Apollo",
-    description:
-      "Apollo Education Group, Inc. is an American corporation that owns several for-profit educational institutions.",
-    image: "apollo.png",
-    link: "/work/apollo",
-  },
-  {
-    title: "Amazon",
-    description:
-      "Amazon is an American multinational technology company that focuses on e-commerce, cloud computing, digital streaming, and artificial intelligence.",
-    image: "amazon.png",
-    link: "/work/amazon",
-  },
-  {
-    title: "Nordstrom",
-    description: "Nordstrom is an American luxury department store chain.",
-    image: "nordstrom.png",
-    link: "/work/nordstrom",
-  },
-  {
-    title: "Disney",
-    description:
-      "The Walt Disney Company, commonly known as Disney, is an American diversified multinational mass media and entertainment conglomerate.",
-    image: "disney.png",
-    link: "/work/disney",
-  },
-  {
-    title: "Real Networks",
-    description:
-      "RealNetworks is a provider of Internet streaming media delivery software and services based in Seattle, Washington, United States.",
-    image: "realnetworks.png",
-    link: "/work/realnetworks",
-  },
-];
+const firstcard = [styles.card, styles.firstcard].join(" ");
 
-const firstworkcard = [styles.workcard, styles.threequartercard].join(" ");
-
-const FirstWorkItem = ({ title, description, image }: WorkItem) => {
+const FirstItem = ({ title, name, description, image }: Item) => {
   return (
     <div className={styles.item}>
       <div className={styles.title}>
-        <h1>WORK</h1>
+        <h1>{title}</h1>
       </div>
-      <div className={firstworkcard}>
-        <div className={styles.toggle}>
-          <span className={styles.expand}></span>
-        </div>
-        <div className={styles.logo}>
-          <Image src={`/work/${image}`} alt={description} fill />
-        </div>
-        <h2>{title}</h2>
-      </div>
-    </div>
-  );
-};
-
-const FirstPlayItem = ({ title, description, image, link }: WorkItem) => {};
-
-const WorkItem = ({ title, description, image }: WorkItem) => {
-  return (
-    <div className={styles.item}>
-      <div className={styles.workcard}>
-        <div className={styles.toggle}>
-          <span className={styles.expand}></span>
-        </div>
+      <div className={firstcard}>
         <div className={styles.logo}>
           <Image
-            src={`/work/${image}`}
+            src={image}
             alt={description}
             style={{ objectFit: "contain" }}
             fill
           />
         </div>
-        <h2>{title}</h2>
+        <h2>{name}</h2>
+      </div>
+    </div>
+  );
+};
+
+const Item = ({ name, description, image }: Item) => {
+  return (
+    <div className={styles.item}>
+      <div className={styles.card}>
+        <div className={styles.logo}>
+          <Image
+            src={image}
+            alt={description}
+            style={{ objectFit: "contain" }}
+            fill
+          />
+        </div>
+        <h2>{name}</h2>
       </div>
     </div>
   );
@@ -116,15 +57,16 @@ const WorkPage = () => {
     <React.Fragment>
       <section className={styles.work}>
         <div className={styles.grid}>
-          <FirstWorkItem
+          <FirstItem
             title={worklist[0].title}
+            name={worklist[0].name}
             description={worklist[0].description}
             image={worklist[0].image}
             link={worklist[0].link}
           />
           {worklist.slice(1).map((work) => (
-            <WorkItem
-              title={work.title}
+            <Item
+              name={work.name}
               description={work.description}
               image={work.image}
               link={work.link}
@@ -133,9 +75,25 @@ const WorkPage = () => {
           ))}
         </div>
       </section>
+
       <section className={styles.play}>
         <div className={styles.grid}>
-          <div className={styles.item}></div>
+          <FirstItem
+            title={playlist[0].title}
+            name={playlist[0].name}
+            description={playlist[0].description}
+            image={playlist[0].image}
+            link={playlist[0].link}
+          />
+          {playlist.slice(1).map((play) => (
+            <Item
+              name={play.name}
+              description={play.description}
+              image={play.image}
+              link={play.link}
+              key={play.title}
+            />
+          ))}
         </div>
       </section>
     </React.Fragment>
