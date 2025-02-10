170958f977f4da3f0244560e192015b4a2fb3d50
Ava Collins <info@avacollins.dev>
2025-02-10 11:50:04 -0500

    feat: chatbot demo integration test

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Script from 'next/script'
 
 import "./globals.css";
 
@@ -21,6 +22,17 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
         <Analytics />
+        <Script id="chat-demo" strategy="afterInteractive">
+          {`
+          (function(d, m){
+            var kommunicateSettings = {"appId":"3395df8f0df14b6325bdbd8eb8580b8a9","popupWidget":true,"automaticChatOpenOnNavigation":true};
+            var s = document.createElement("script"); s.type = "text/javascript"; s.async = true;
+            s.src = "https://widget.kommunicate.io/v2/kommunicate.app";
+            var h = document.getElementsByTagName("head")[0]; h.appendChild(s);
+            window.kommunicate = m; m._globals = kommunicateSettings;
+          })(document, window.kommunicate || {});
+        `}
+        </Script>
       </body>
     </html>
   );
